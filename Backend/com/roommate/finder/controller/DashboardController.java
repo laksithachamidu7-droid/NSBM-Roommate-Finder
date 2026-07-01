7a134a953bd0adac8f5129bebda09262d4fb8956
Naveen-Udana <naveenudana123@gmail.com>
2026-07-01 15:08:32 +0530

    Create DashboardController.java
    added dashboardcontroller exposing GET mapping for metrics endpoints

---
+++ Backend/com/roommate/finder/controller/DashboardController.java
@@ -0,0 +1,23 @@
+package com.roommate.finder.controller;
+
+import com.roommate.finder.dto.DashboardStats;
+import com.roommate.finder.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+public class DashboardController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping("/stats")
+    public ResponseEntity<DashboardStats> getStats() {
+        DashboardStats stats = dashboardService.getDashboardStats();
+        return ResponseEntity.ok(stats);
+    }
+}
